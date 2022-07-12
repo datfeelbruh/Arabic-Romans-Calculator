632f8e80ed77aabd25b8d43a48db5b496eee9a72
khoroshko.d <datfeelman@gmail.com>
2022-07-12 16:58:13 +0300

    add description

---
+++ src/main/java/CalculatorView/ConsoleVIew.java
@@ -0,0 +1,11 @@
+package CalculatorView;
+
+public class ConsoleVIew {
+    private final String DESCRIPTION = "Калькулятор для выражений вида:"
+                                        + " \"Число1 Операция Число2\", через пробел."
+                                        +" Допускаются числа от 1 до 10 или от I до X включительно. "
+                                        + "Операции: + - * /";
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
