a57956fb5f5f3d26dbf1d4ef5e7112b4971afa09
khoroshko.d <datfeelman@gmail.com>
2022-07-12 22:03:21 +0300

    add logic

--- src/main/java/validation/Validator.java
+++ src/main/java/validation/Validator.java
@@ -1,4 +1,34 @@
 package validation;
 
+import converter.ConvertToArabic;
+
+import java.util.Arrays;
+
 public class Validator {
+    private static final String[] OPERATORS = {"+", "-", "*", "/"};
+
+    public static boolean isCorrectInputNumber(String number) throws IllegalArgumentException {
+        return !isArabic(number) && !isRoman(number);
+    }
+    public static boolean isCorrectOperator(String operator) {
+        return Arrays.asList(OPERATORS).contains(operator);
+    }
+
+    private static boolean isArabic(String number){
+        try {
+            Integer.parseInt(number);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isRoman(String number) {
+        try {
+            ConvertToArabic.getArabic(number);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+        return true;
+    }
 }
